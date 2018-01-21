ef9f52955e90722eb0fd4781111ef3e4b37f9a72
ak1103dev <ak.apichan1103@gmail.com>
2018-01-21 10:10:40 +0700

    :construction: show line chart

--- src/containers/chart.js
+++ src/containers/chart.js
@@ -3,13 +3,14 @@ import {
   StyleSheet,
   View
 } from 'react-native';
+import Line from '../components/chart/line';
 
 export default class Chart extends Component {
 
   render() {
     return (
       <View style={styles.container}>
-
+        <Line values={[40, 30, 70, 60, 100, 70, 40, 70, 50]} />
       </View>
     );
   }
